7d96d579d32a76d6162ac309b51ae548a033eea3
Holland Graves <zacharyledoux13@gmail.com>
2018-10-14 21:53:44 -0600

    finished small program on maps to test functionality of syntax

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 // IMPORTS GO HERE
 
+import "fmt"
+
 // STRUCTS GO HERE
 
 // MAPS GO HERE
@@ -9,7 +11,18 @@ package main
 // MAIN GOES HERE
 
 func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#008000",
+		"white": "#ffffff",
+	}
 
+	printMap(colors)
 }
 
 // USER DEFINED FUNCTIONS GO HERE
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
